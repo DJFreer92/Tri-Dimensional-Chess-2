2aec0bfaa0a9afb764a7e9083ae76d12967a8bdb
Daniel Freer <deletes_minimal.0w@icloud.com>
2024-05-21 04:50:57 -0400

    Added new move events

--- Assets/Scripts/Game/Moves/MoveEvent.cs
+++ Assets/Scripts/Game/Moves/MoveEvent.cs
@@ -15,20 +15,29 @@ namespace TriDimensionalChess.Game.Moves {
 		SECONDARY_PROMOTION = 1 << 8,
 		ATTACK_BOARD_CLAIM = 1 << 9,
 		ATTACK_BOARD_ROTATE = 1 << 10,
-		CHECK = 1 << 11,
-		CHECKMATE = 1 << 12 | CHECK,
-		DRAW_OFFERED = 1 << 13,
-		CASTLING = (CASTLING_KING_SIDE | CASTLING_QUEEN_SIDE) & ~LOST_CASTLING_RIGHTS
+		ATTACK_BOARD_INVERSION = 1 << 11,
+		PROMOTION_TO_ATTACK_BOARD = 1 << 12,
+		CHECK = 1 << 13,
+		CHECKMATE = 1 << 14 | CHECK,
+		DRAW_OFFERED = 1 << 15,
+		CASTLING = (CASTLING_KING_SIDE | CASTLING_QUEEN_SIDE) & ~LOST_CASTLING_RIGHTS,
+		ANY_PROMOTION = PROMOTION | SECONDARY_PROMOTION | PROMOTION_TO_ATTACK_BOARD,
+		LOST_RIGHTS = LOST_DOUBLE_SQUARE_MOVE_RIGHTS | LOST_CASTLING_RIGHTS
 	}
 
 	public static class MoveEventExtensions {
 		///<summary>
-		///Returns whether the given category contains the given event
+		///Returns whether the given category contains the given event(s)
 		///</summary>
-		///<params name="category">The category to check for the event in</params>
-		///<params name="moveEvent">The event to look for in the category</params>
-		///<returns>Whether the given category contains the given event</returns>
-		public static bool Contains(this MoveEvent category, MoveEvent moveEvent) => (category & moveEvent) == moveEvent;
+		///<params name="category">The category to check for the event(s) in</params>
+		///<params name="moveEvents">The event(s) to look for in the category</params>
+		///<returns>Whether the given category contains the given event(s)</returns>
+		public static bool Contains(this MoveEvent category, params MoveEvent[] moveEvents) {
+			if (moveEvents == null) throw new ArgumentNullException(nameof(moveEvents), "MoveEvents cannot be null");
+			foreach (MoveEvent evnt in moveEvents)
+				if ((category & evnt) != evnt) return false;
+			return true;
+		}
 
 		///<summary>
 		///Returns whether the given category contains the any of the given event
@@ -39,13 +48,23 @@ namespace TriDimensionalChess.Game.Moves {
 		public static bool PartialContains(this MoveEvent category, MoveEvent moveEvent) => (category & moveEvent) > 0;
 
 		///<summary>
-		///Adds the given event to the referenced event
+		///Adds the given event(s) to the referenced event
 		///</summary>
-		///<params name="moveEventRef">A reference to an event to add the given event to</params>
-		///<params name="moveEvent">The event to add to the given event reference</params>
+		///<params name="moveEventRef">A reference to an event to add the given event(s) to</params>
+		///<params name="moveEvent">The event(s) to add to the given event reference</params>
 		public static void Add(this ref MoveEvent moveEventRef, params MoveEvent[] moveEvents) {
 			if (moveEvents == null) throw new ArgumentNullException(nameof(moveEvents), "MoveEvents cannot be null");
 			foreach (MoveEvent other in moveEvents) moveEventRef |= other;
+		}
+
+		///<summary>
+		///Removes the given event(s) from the referenced events
+		///</summary>
+		///<param name="moveEventRef">A reference to an event to add the given event(s) to</param>
+		///<param name="moveEvents">The event(s) to add to the given event reference</param>
+		public static void Remove(this ref MoveEvent moveEventRef, params MoveEvent[] moveEvents) {
+			if (moveEvents == null) throw new ArgumentNullException(nameof(moveEvents), "MoveEvents cannot be null");
+			foreach (MoveEvent other in moveEvents) moveEventRef &= ~other;
 		}
 
 		///<summary>
