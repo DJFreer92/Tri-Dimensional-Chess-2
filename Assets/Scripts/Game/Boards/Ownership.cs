77f46f4277d63c1be23ffaf909599078b5756ca2
Daniel Freer <deletes_minimal.0w@icloud.com>
2024-05-21 04:53:20 -0400

    Added ownership extension methods

--- Assets/Scripts/Game/Boards/Ownership.cs
+++ Assets/Scripts/Game/Boards/Ownership.cs
@@ -1,8 +1,26 @@
+using static TriDimensionalChess.Game.Boards.Ownership;
+
 namespace TriDimensionalChess.Game.Boards {
-    public enum Ownership {
-        NONE = 0,
-        NEUTRAL = 'N',
-        WHITE = 'W',
-        BLACK = 'B'
-    }
+	public enum Ownership {
+		NEUTRAL = 'N',
+		WHITE = 'W',
+		BLACK = 'B'
+	}
+
+	public static class OwnershipExtentions {
+		///<summary>
+		///Returns whether the owner is white or black
+		///</summary>
+		///<param name="owner">the owner</param>
+		///<returns>Whether the owner is white or black</returns>
+		public static bool IsWhiteOrBlack(this Ownership owner) => owner != NEUTRAL;
+
+		///<summary>
+		///Returns whether the owner matches the boolean value of whether the player is white or black
+		///</summary>
+		///<param name="owner">The owner</param>
+		///<param name="player">White or black</param>
+		///<returns>Whether the owner matches the boolean value</returns>
+		public static bool MatchesPlayerBool(this Ownership owner, bool player) => (player ? WHITE : BLACK) == owner;
+	}
 }
