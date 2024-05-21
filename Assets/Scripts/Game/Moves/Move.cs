bbf06ff99e8f220b5ac31b922025325141e5d9d7
Daniel Freer <deletes_minimal.0w@icloud.com>
2024-05-21 04:51:56 -0400

    Updated move builder for attack board rotating and inverting

--- Assets/Scripts/Game/Moves/Move.cs
+++ Assets/Scripts/Game/Moves/Move.cs
@@ -4,7 +4,6 @@ using System.Text;
 
 using TriDimensionalChess.Game.Boards;
 using TriDimensionalChess.Game.ChessPieces;
-using TriDimensionalChess.Game.Moves;
 using TriDimensionalChess.Game.Notation;
 
 namespace TriDimensionalChess.Game.Moves {
@@ -13,26 +12,26 @@ namespace TriDimensionalChess.Game.Moves {
 		public readonly Player Player;
 		//the departure and arrival squares
 		public readonly Square StartSqr, EndSqr;
-		//what events occured during the move
-		public MoveEvent MoveEvents;
-		//whether the move is to be formatted in figurine notation
-		public bool UseFigurineNotation;
 		//what type of piece the user has selected to promote a pawn to
 		public PieceType Promotion = PieceType.NONE;
 		//what type of piece the secondary promotion is to
 		public PieceType SecondaryPromotion = PieceType.NONE;
 		//holder for pawn or promoted piece for when a promotion is undone or redone
 		public ChessPiece PromotionUndoRedoHolder;
+
 		//the square the pawn being en passant is on
 		protected Square _enPassantCaptureSqr;
 		//the notation of the square being departed from for the purposes of short algebraic move notation
 		protected string _departureNotation;
 
-		public Move(Player player, Square start, Square end) {
+		//what events occured during the move
+		private MoveEvent _moveEvents;
+
+		public Move(Player player, Square start, Square end, MoveEvent moveEvents = MoveEvent.NONE) {
 			Player = player;
 			StartSqr = start;
 			EndSqr = end;
-			UseFigurineNotation = SettingsManager.Instance.FigurineNotation;
+			_moveEvents = moveEvents;
 		}
 
 		///<summary>
@@ -51,21 +50,45 @@ namespace TriDimensionalChess.Game.Moves {
 		public abstract void Redo();
 
 		///<summary>
-		///Returns the notation of the move in long 3D chess algebraic notation
-		///</summary>
-		///<returns>The notation of the move in long 3D chess algebraic notation</returns>
-		public abstract string GetLongNotation();
-
-		///<summary>
 		///Returns the notation of the move in short 3D chess algebraic notation
 		///</summary>
 		///<returns>The notation of the move in short 3D chess algebraic notation</returns>
-		public abstract string GetShortNotation();
+		public abstract string GetNotation();
 
 		///<summary>
 		///Determine the departure notation of the move
 		///</summary>
 		protected abstract void DetermineDepartureNotation();
+
+		///<summary>
+		///Returns the events that occur during the move
+		///</summary>
+		///<returns>The events that occur during the move</returns>
+		public MoveEvent GetMoveEvents() => _moveEvents;
+
+		///<summary>
+		///Adds the given move event(s) to the move events
+		///</summary>
+		///<param name="events">The move event(s) to add</param>
+		public void AddMoveEvent(params MoveEvent[] events) => _moveEvents.Add(events);
+
+		///<summary>
+		///Removes the given move event(s) from the move events
+		///</summary>
+		///<param name="events">The move event(s) to be removed</param>
+		public void RemoveMoveEvent(params MoveEvent[] events) => _moveEvents.Remove(events);
+
+		///<summary>
+		///Returns whether the move has the given move event(s)
+		///</summary>
+		///<param name="events">The move event(s) to check for</param>
+		public bool HasMoveEvent(params MoveEvent[] events) => _moveEvents.Contains(events);
+
+		///<summary>
+		///Returns whether the move has a move event of the given category of move events
+		///</summary>
+		///<param name="evnt">Category of move events</param>
+		public bool HasMoveEventOf(MoveEvent evnt) => _moveEvents.PartialContains(evnt);
 
 		///<summary>
 		///Build the move from the given notation
@@ -74,80 +97,6 @@ namespace TriDimensionalChess.Game.Moves {
 		///<param name="isWhite">Whether the player is white</param>
 		///<returns>The built move</returns>
 		public static Move BuildMove(string notatedMove, bool isWhite) {
-			Debug.Log($"Building {notatedMove}");
-			Move move;
-
-			int seperationIndex, endIndex = notatedMove.Length;
-			if (notatedMove.Contains('=')) endIndex = notatedMove.IndexOf('=');
-			else if (notatedMove.Contains('+') || notatedMove.Contains('#')) endIndex--;
-
-			if (notatedMove.Contains("O-O")) {
-				Square rookSqr;
-
-				if (notatedMove.Contains("O-O-O"))
-					rookSqr = ChessBoard.Instance.GetSquareAt(
-						isWhite ? ChessBoard.WhiteQueenSideRookCoords : ChessBoard.BlackQueenSideRookCoords
-					);
-				else
-					rookSqr = ChessBoard.Instance.GetSquareAt(
-						isWhite ? ChessBoard.WhiteKingSideRookCoords : ChessBoard.BlackKingSideRookCoords
-					);
-
-				move = new PieceMove(
-					Game.Instance.GetPlayer(isWhite),
-					ChessBoard.Instance.GetSquareAt(isWhite ? ChessBoard.WhiteKingCoords : ChessBoard.BlackKingCoords),
-					rookSqr
-				);
-			} else if (notatedMove.Contains('-')) {
-				seperationIndex = notatedMove.IndexOf('-');
-				if (char.IsUpper(notatedMove[endIndex - 1])) endIndex--;
-
-				move = new AttackBoardMove(
-					Game.Instance.GetPlayer(isWhite),
-					ChessBoard.Instance.GetSquareAt(notatedMove[..seperationIndex].BoardToVector() + Vector3Int.up),
-					ChessBoard.Instance.GetSquareAt(notatedMove[(seperationIndex + 1)..endIndex].BoardToVector())
-				);
-			} else {
-				seperationIndex = notatedMove.IndexOf('x');
-				if (seperationIndex == -1)
-					for (int i = 2; i < notatedMove.Length; i++)
-						if (char.IsLower(notatedMove[i])) seperationIndex = i;
-				if (notatedMove.Contains("e.p.")) endIndex = notatedMove.IndexOf("e.p.");
-
-				move = new PieceMove(
-					Game.Instance.GetPlayer(isWhite),
-					ChessBoard.Instance.GetSquareAt(
-						notatedMove[(char.IsUpper(notatedMove[0]) ? 1 : 0)..seperationIndex].NotationToVector()
-					),
-					ChessBoard.Instance.GetSquareAt(
-						notatedMove[(seperationIndex + (notatedMove.Contains('x') ? 1 : 0))..endIndex].NotationToVector()
-					)
-				);
-			}
-
-			if (notatedMove.Contains('x')) move.MoveEvents.Add(MoveEvent.CAPTURE);
-			if (notatedMove.Contains("e.p.")) move.MoveEvents.Add(MoveEvent.EN_PASSANT);
-			else if (notatedMove.Contains('=') || (notatedMove.Contains('-') && !notatedMove.Contains("O-O") && char.IsUpper(notatedMove[^1]))) {
-				move.MoveEvents.Add(MoveEvent.PROMOTION);
-				move.Promotion = notatedMove[^1].CharToPiece();
-			}
-			if (notatedMove.Contains('#')) move.MoveEvents.Add(MoveEvent.CHECK, MoveEvent.CHECKMATE);
-			else if (notatedMove.Contains('+')) {
-				move.MoveEvents.Add(MoveEvent.CHECK);
-				ChessBoard.Instance.GetKing(!isWhite).IsInCheck = true;
-			}
-
-			return move;
-		}
-
-		///<summary>
-		///Build the move from the given notation
-		///</summary>
-		///<param name="notatedMove">Notation of the move to be built</param>
-		///<param name="isWhite">Whether the player is white</param>
-		///<returns>The built move</returns>
-		public static Move BuildMoveDynamic(string notatedMove, bool isWhite) {
-			Debug.Log($"Dynamically Building {notatedMove}");
 			Move move;
 			Square startSqr = null, endSqr = null;
 			bool isAttackBoardMove = false, hasCheckChar = false;
@@ -161,7 +110,11 @@ namespace TriDimensionalChess.Game.Moves {
 			else if (hasCheckChar) endIndex--;
 			else if (notatedMove.Contains("(=)")) endIndex = notatedMove.IndexOf("(=)");
 
-			if (notatedMove.Contains("O-O")) {  //castling move
+			if (notatedMove.Contains('⟳')) {  //attack board rotation
+				isAttackBoardMove = true;
+				startSqr = endSqr = ChessBoard.Instance.GetSquareAt(notatedMove[1..4].BoardToVector() + Vector3Int.up);
+				if (char.IsUpper(notatedMove[endIndex - 1])) endIndex--;
+			} else if (notatedMove.Contains("O-O")) {  //castling move
 				Square rookSqr;
 
 				if (notatedMove.Contains("O-O-O"))
@@ -199,11 +152,6 @@ namespace TriDimensionalChess.Game.Moves {
 
 				if (pieceType != PieceType.PAWN) startIndex++;
 
-				Debug.Log(pieceType);
-
-				Debug.Log($"Start Index: {startIndex}");
-				Debug.Log($"End Index: {endIndex}");
-
 				string departureInfo = "", arrivalInfo = "";
 				for (int i = startIndex; i < endIndex; i++) {
 					if (notatedMove[i] == 'x') {
@@ -218,16 +166,12 @@ namespace TriDimensionalChess.Game.Moves {
 					arrivalInfo += notatedMove[i];
 				}
 
-				Debug.Log(departureInfo);
-				Debug.Log(arrivalInfo);
-
 				endSqr = ChessBoard.Instance.GetSquareAt(arrivalInfo.NotationToVector());
 
 				if (departureInfo.Length >= 3 && char.IsDigit(departureInfo[1]))
 					startSqr = ChessBoard.Instance.GetSquareAt(arrivalInfo.NotationToVector());
 
 				foreach (ChessPiece piece in ChessBoard.Instance.GetPieces()) {
-					Debug.Log($"Found piece of type: {piece.Type}");
 					if (piece.Type != pieceType) continue;
 					if (!string.IsNullOrEmpty(departureInfo) && !piece.GetSquare().Notation.Contains(departureInfo[0])) continue;
 					if (departureInfo.Length > 1 && !piece.GetSquare().Notation.Contains(departureInfo[1..])) continue;
@@ -242,31 +186,31 @@ namespace TriDimensionalChess.Game.Moves {
 				}
 			}
 
-			Debug.Log($"Start Square: {(startSqr == null ? "NULL" : startSqr.Notation)}");
-			Debug.Log($"End Square: {(endSqr == null ? "NULL" : endSqr.Notation)}");
+			if (endIndex == notatedMove.Length) endIndex--;
 
 			move = isAttackBoardMove ?
 				new AttackBoardMove(Game.Instance.GetPlayer(isWhite), startSqr, endSqr) :
 				new PieceMove(Game.Instance.GetPlayer(isWhite), startSqr, endSqr);
 
-			if (notatedMove.Contains('x')) move.MoveEvents.Add(MoveEvent.CAPTURE);
+			if (notatedMove.Contains('x')) move.AddMoveEvent(MoveEvent.CAPTURE);
+			else if (notatedMove.Contains('⟳')) move.AddMoveEvent(MoveEvent.ATTACK_BOARD_ROTATE);
 
-			if (notatedMove.Contains("e.p.")) move.MoveEvents.Add(MoveEvent.EN_PASSANT);
+			if (notatedMove.Contains("e.p.")) move.AddMoveEvent(MoveEvent.EN_PASSANT);
 			else if (notatedMove.IndexOf('=') != notatedMove.IndexOf("(=)")) {
-				move.MoveEvents.Add(MoveEvent.PROMOTION);
+				move.AddMoveEvent(MoveEvent.PROMOTION);
 				move.Promotion = notatedMove[endIndex + 1].CharToPiece();
 			} else if (notatedMove.Contains('/')) {
-				move.MoveEvents.Add(MoveEvent.SECONDARY_PROMOTION);
+				move.AddMoveEvent(MoveEvent.SECONDARY_PROMOTION);
 				move.SecondaryPromotion = notatedMove[0].CharToPiece();
 				(move as AttackBoardMove).AutoReExecute = true;
 			} else if (isAttackBoardMove && char.IsUpper(notatedMove[endIndex])) {
-				move.MoveEvents.Add(MoveEvent.PROMOTION);
+				move.AddMoveEvent(MoveEvent.PROMOTION);
 				move.Promotion = notatedMove[endIndex].CharToPiece();
 			}
 
-			if (notatedMove.Contains('#')) move.MoveEvents.Add(MoveEvent.CHECKMATE);
+			if (notatedMove.Contains('#')) move.AddMoveEvent(MoveEvent.CHECKMATE);
 			else if (notatedMove.Contains('+')) {
-				move.MoveEvents.Add(MoveEvent.CHECK);
+				move.AddMoveEvent(MoveEvent.CHECK);
 				ChessBoard.Instance.GetKing(!isWhite).IsInCheck = true;
 			}
 
@@ -308,9 +252,9 @@ namespace TriDimensionalChess.Game.Moves {
 		///<returns>The ending notation of the move</returns>
 		public string GetEndingNotation() {
 			var move = new StringBuilder();
-			if (MoveEvents.Contains(MoveEvent.DRAW_OFFERED)) move.Append("=");  //draw offered
-			if (MoveEvents.Contains(MoveEvent.CHECKMATE)) move.Append("#");  //checkmate
-			else if (MoveEvents.Contains(MoveEvent.CHECK)) move.Append("+");  //check
+			if (HasMoveEvent(MoveEvent.DRAW_OFFERED)) move.Append("=");  //draw offered
+			if (HasMoveEvent(MoveEvent.CHECKMATE)) move.Append("#");  //checkmate
+			else if (HasMoveEvent(MoveEvent.CHECK)) move.Append("+");  //check
 			return move.ToString();
 		}
 
@@ -320,11 +264,11 @@ namespace TriDimensionalChess.Game.Moves {
 		///<returns>Data about the move</returns>
 		public override string ToString() {
 			var str = new StringBuilder(base.ToString());
-			str.Append("\nNotation: ").Append(GetLongNotation());
+			str.Append("\nNotation: ").Append(GetNotation());
 			str.Append("\nStartSqr Square: ").Append(StartSqr.ToString());
 			str.Append("\nEndSqr Square: ").Append(EndSqr.ToString());
 			str.Append("\nIs Attackboard Move? ").Append(this is AttackBoardMove);
-			str.Append("\nEvents: ").Append(MoveEvents.ToString());
+			str.Append("\nEvents: ").Append(_moveEvents.ToString());
 			return str.ToString();
 		}
 	}
